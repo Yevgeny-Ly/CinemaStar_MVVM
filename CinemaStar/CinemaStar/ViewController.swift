266bdab2deab7110c2eb5687acbf9c98b4f7165f
Yevgeny Ly <globchenko16@gmail.com>
2024-04-22 13:52:19 +0300

    - app init

---
+++ CinemaStar/CinemaStar/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  CinemaStar
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
